48464d1d8f45fb584943376b4d509cfc17075994
Al <alchew2000@gmail.com>
2022-04-06 17:36:12 -0700

    first initial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 function App() {
 
   return (
     <div className="App">
         <h1>Hello React App for a page only</h1>
-        <p style={{fontSize: 30}}>Lorem</p>
+        
     </div>
   );
 };
